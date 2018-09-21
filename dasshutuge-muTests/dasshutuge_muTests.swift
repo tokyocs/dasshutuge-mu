fd638f2066793756c9c3809b757876ca32bc9fef
tcs16170 <tcs16170@tcs-mac-1801.local>
2018-09-21 15:26:57 +0900

    Initial Commit

---
+++ dasshutuge-muTests/dasshutuge_muTests.swift
@@ -0,0 +1,36 @@
+//
+//  dasshutuge_muTests.swift
+//  dasshutuge-muTests
+//
+//  Created by tcs16170 on 2018/09/21.
+//  Copyright © 2018年 tcs16170. All rights reserved.
+//
+
+import XCTest
+@testable import dasshutuge_mu
+
+class dasshutuge_muTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
